c8c0054ffdf4c320affa97ebfa0624bc4a3d96ca
MonarchButterflySquad <91906577+AnthonyVicenti@users.noreply.github.com>
2023-10-19 12:28:15 -0700

    Revert "test"
    This reverts commit 116fdb9e473f66bcc5cf31fea98dbb362c91c64c.

--- Mint/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-// Updated 13 Jul to Mages Staking Theming
-
-export { Container } from "./Container";
-export { Navbar } from "./Navbar";
-export { Footer } from "./Footer";
-export {NFTGrid} from "./NFTGrid"
